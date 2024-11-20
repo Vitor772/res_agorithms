b2d47a18b0f942ea47e512ca365b17d40b642ace
Vitor772 <vitor989santos@gmail.com>
2024-11-20 19:51:46 -0300

    20/11/2024

---
+++ longestCommonPrefix.py
@@ -0,0 +1,23 @@
+def longestCommonPrefix(strs):
+    if not strs:
+        return ""
+
+    prefix = strs[0]
+
+    for string in strs[1:]:
+
+        while not string.startswith(prefix):
+
+            prefix = prefix[:-1]
+            if not prefix:
+                return ""
+
+    return prefix
+
+strs = ["flower","flow","flight"]
+
+resultado = longestCommonPrefix(strs)
+
+print(resultado)
+
+
